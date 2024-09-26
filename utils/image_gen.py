dc1d7ccbd60fedecd1a11ded67365b602ba56e7a
zen86b <tuan.work15118@gmail.com>
2024-09-26 18:48:19 +0700

    Update image_gen.py

--- utils/image_gen.py
+++ utils/image_gen.py
@@ -15,7 +15,7 @@ def image_generate(
     negative_prompt=None,
     height=1280,  # Đặt kích thước dọc 720x1280
     width=720,
-    num_inference_steps=50,
+    num_inference_steps=20,
     guidance_scale=3.5,
     num_images_per_prompt=1,
     seed=-1,
